b53775acd608c96d2b80c0269d7afb1f990c780c
Repository with test projects <teamcity.qa.server@gmail.com>
2017-03-15 23:11:58 +0300

    Update Powershell_Arguments_PowerShell20.kt

--- .teamcity/Powershell_Arguments/buildTypes/Powershell_Arguments_PowerShell20.kt
+++ .teamcity/Powershell_Arguments/buildTypes/Powershell_Arguments_PowerShell20.kt
@@ -3,7 +3,7 @@ package Powershell_Arguments.buildTypes
 import jetbrains.buildServer.configs.kotlin.v10.*
 
 object Powershell_Arguments_PowerShell20 : BuildType({
-    template(Powershell_Arguments.buildTypes.Powershell_Arguments_Arguments_2)
+    //template(Powershell_Arguments.buildTypes.Powershell_Arguments_Arguments_2)
     uuid = "62d37198-9144-490f-a56e-f068a002d30b"
     extId = "Powershell_Arguments_PowerShell20"
     name = "PowerShell 2.0"
