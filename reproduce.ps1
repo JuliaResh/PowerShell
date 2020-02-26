34213e85cd5ae4a2a3bc06fa3e9c5c91e0c22469
Repository with test projects <teamcity.qa.server@gmail.com>
2020-02-26 16:27:41 +0300

    Update reproduce.ps1

--- reproduce.ps1
+++ reproduce.ps1
@@ -1,4 +1,5 @@
 $ErrorActionPreference = "stop"
+$PSVersionTable
 python -c "import sys; print('hello from stderr 1', file=sys.stderr)"
 python -c "import sys; print('hello from stderr 2', file=sys.stderr)"
 python -c "import sys; print('hello from stderr 3', file=sys.stderr)"
